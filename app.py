e68c4b91895e46bfa7d0ccc0c44d537d5b3c0e64
Ganesh Kutty <ganeshkutty2257767@gmail.com>
2023-02-04 22:53:34 +0530

    Revert "Added play functionality"
    This reverts commit 0d371d5a5c67c9f9f0075a026ad604e131be2832.

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
-from flask import Flask, render_template, flash, redirect, url_for, request
+from flask import Flask, render_template, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_login import LoginManager, UserMixin, login_user, current_user, login_required, logout_user
 # from werkzeug.security import generate_password_hash, check_password_hash
+import os
 from datetime import datetime
 from forms import RegistrationForm, LoginForm
 from sqlalchemy.exc import IntegrityError, PendingRollbackError
-import os, random, base64, io, math
-from PIL import Image
+import os
+import gunicorn
 
-MYSQLDATABASE = os.getenv("MYSQLDATABASE")
-MYSQLHOST = os.getenv("MYSQLHOST")
-MYSQLPASSWORD = os.getenv("MYSQLPASSWORD")
-MYSQLPORT = os.getenv("MYSQLPORT")
-MYSQLUSER = os.getenv("MYSQLUSER")
 
 SECRET_KEY = os.urandom(32)
 
 app = Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{MYSQLUSER}:{MYSQLPASSWORD}@{MYSQLHOST}:{MYSQLPORT}/{MYSQLDATABASE}"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:cAII3XUWDR7bO2MeDXA7@containers-us-west-100.railway.app:5710/railway"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = SECRET_KEY
@@ -30,68 +26,18 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-def process_image(raw_data):
-    ratio = 0.5
-    buffer = io.BytesIO() # Create a Bytes IO buffer
-    raw_data = Image.open(raw_data)
-    w, h = raw_data.size 
-    resized_image = raw_data.resize((math.floor(w*ratio), math.floor(h*ratio)), Image.Resampling.LANCZOS)
-    resized_image.save(buffer, format="PNG", optimize=True, quality=85)
-    downloadable_data = buffer.getvalue()
-    
-    resized_image = raw_data.resize((100, 100), Image.Resampling.LANCZOS)
-    resized_image.save(buffer, format="PNG", optimize=True, quality=85)
-    renderable_data = buffer.getvalue()
-    renderable_data = base64.b64encode(renderable_data).decode('ascii')
-    
-    return downloadable_data, renderable_data
-
-
-def get_user_details():
-    return User.query.with_entities(User.first_name, User.last_name, User.email).all()
-
-
-def send_emails(form):
-    form = form.to_dict(flat=False)
-    mappings = list(zip(form['to'], form['from']))
-
-
-    for mapping in mappings:
-        user = User.query.filter_by(email=mapping[0]).first()
-        print(f"""\n\nYou will send your gift to {user.first_name} {user.last_name}.
-              Find their contact details below:
-              Name: {user.first_name} {user.last_name}.
-              Address: {user.address}.
-              Mobile Number: {user.mobile_number}.
-              Email ID: {user.email}
-
-              (Send this email to {mapping[1]})""")
-
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
-    employee_id = db.Column(db.BigInteger, unique=True, index=True)
+    employee_id = db.Column(db.Integer, unique=True, index=True)
     email = db.Column(db.String(150), unique = True, index = True)
-    address = db.Column(db.Text, nullable=False)
-    mobile_number = db.Column(db.BigInteger, unique=True, nullable=False)
     joined_at = db.Column(db.DateTime(), default = datetime.utcnow, index = True)
-    gift = db.relationship('Gift', backref='user', uselist=False)
     
     def __repr__(self):
         return f"{self.employee_id} - {self.email}"
 
-
-class Gift(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    gift_image_download = db.Column(db.LargeBinary(length=(2**32)-1), nullable=False)
-    gift_image_render = db.Column(db.Text(length=(2**32)-1), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    
-    def __repr__(self):
-        return f"""Gift from: {self.user_id.email_id}"""
-    
     # def set_password(self, password):
     #     self.password_hash = generate_password_hash(password)
 
@@ -101,38 +47,26 @@ class Gift(db.Model):
 
 @login_manager.user_loader
 def load_user(user_id):
+    print(user_id)
+    print(User.query.get(user_id))
     return User.query.get(user_id)
 
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    users = get_user_details()
-    return render_template('dashboard.html', total=len(users), users=users)
+    return render_template('dashboard.html', current_user=current_user)
 
 
 @app.route('/', methods = ['POST','GET'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        
-        downloadable_image, renderable_image = process_image(form.gift_image.data)
-        
-        user = User(first_name = form.first_name.data,
-                    last_name = form.last_name.data,
-                    employee_id=form.employee_id.data,
-                    email = form.email.data,
-                    address = form.address.data,
-                    mobile_number = form.mobile_number.data)
-        gift = Gift(gift_image_download = downloadable_image,
-                    gift_image_render = renderable_image,
-                    user = user)
-
+        user = User(first_name = form.first_name.data, last_name = form.last_name.data, employee_id=form.employee_id.data, email = form.email.data)
+        # user.set_password(form.password.data)
         db.session.add(user)
-        db.session.add(gift)
         try:
             db.session.commit()
-        except IntegrityError as e:
-            print(e)
+        except IntegrityError:
             flash("User with this Email ID  or Employee ID is already registered.", category="danger")
             return redirect(url_for('register'))
         except PendingRollbackError:
@@ -164,20 +98,6 @@ def logout():
     logout_user()
     flash("Logged out successfully.", "info")
     return redirect(url_for('login'))
-
-
-@app.route('/play', methods=['GET', 'POST'])
-@login_required
-def play():
-    if request.method == 'GET':
-        participants = User.query.all()
-        gifts = Gift.query.all()
-        random.shuffle(gifts)
-        random.shuffle(participants)
-        return render_template('play.html', participants=participants, gifts=gifts)
-    else:
-        send_emails(request.form)
-        return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,24 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField
-from wtforms.validators import InputRequired, DataRequired, Email, Regexp, Length
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import InputRequired, DataRequired, Email, Regexp
 
 class RegistrationForm(FlaskForm):
-    employee_id = StringField('Employee ID',
-                              validators=[InputRequired(),
-                                          DataRequired(),
-                                          Regexp(regex='^[0-9]+$',
-                                                 message="Use numbers only.")])
+    employee_id = StringField('Employee ID', validators=[InputRequired(), DataRequired(), Regexp(regex='^[0-9]+$', message="Use numbers only.")])
     first_name = StringField('First Name', validators = [InputRequired(), DataRequired()])
     last_name = StringField('Last Name', validators = [InputRequired(), DataRequired()])
-    email = StringField('Email', validators= [InputRequired(), DataRequired(), Email(), Regexp(regex="^[a-zA-Z]+[.][a-zA-Z]+[0-9]*@?(amdocs)\\.com$", message="Email should be of the format 'Name.Surname@Amdocs.com'")])
-    address = StringField('Address',
-                          validators=[DataRequired()], default='A-24, Karan Gharonda, Sainikwadi, Vadgaonsheri, Pune 411014')
-    mobile_number = StringField('Mobile Number',
-                                 validators=[DataRequired(), Length(min=10, max=10), Regexp('\d{10}', message='Only 10 digit numbers')],
-                                 default='9595959700')
-    gift_image = FileField('Upload gift image', validators=[InputRequired(), DataRequired()])
+    email = StringField('Email', validators= [InputRequired(), DataRequired(), Email(), Regexp(regex="^[a-zA-Z]+.?[a-zA-Z]+[0-9]?@?(amdocs)\\.com$", message="Email should be of the format Name.Surname@Amdocs.com")])
     submit = SubmitField('Register')
-
 
 class LoginForm(FlaskForm):
     email = StringField('Email',validators=[InputRequired(), DataRequired(), Email()])
