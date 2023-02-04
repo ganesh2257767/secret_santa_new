dd1c6206c633eacf7f0d30287868010cae9e43cf
Ganesh Kutty <ganeshkutty2257767@gmail.com>
2023-02-04 22:55:46 +0530

    Revert "Revert "Added play functionality""
    This reverts commit e68c4b91895e46bfa7d0ccc0c44d537d5b3c0e64.

--- forms.py
+++ forms.py
@@ -1,13 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import InputRequired, DataRequired, Email, Regexp
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField
+from wtforms.validators import InputRequired, DataRequired, Email, Regexp, Length
 
 class RegistrationForm(FlaskForm):
-    employee_id = StringField('Employee ID', validators=[InputRequired(), DataRequired(), Regexp(regex='^[0-9]+$', message="Use numbers only.")])
+    employee_id = StringField('Employee ID',
+                              validators=[InputRequired(),
+                                          DataRequired(),
+                                          Regexp(regex='^[0-9]+$',
+                                                 message="Use numbers only.")])
     first_name = StringField('First Name', validators = [InputRequired(), DataRequired()])
     last_name = StringField('Last Name', validators = [InputRequired(), DataRequired()])
-    email = StringField('Email', validators= [InputRequired(), DataRequired(), Email(), Regexp(regex="^[a-zA-Z]+.?[a-zA-Z]+[0-9]?@?(amdocs)\\.com$", message="Email should be of the format Name.Surname@Amdocs.com")])
+    email = StringField('Email', validators= [InputRequired(), DataRequired(), Email(), Regexp(regex="^[a-zA-Z]+[.][a-zA-Z]+[0-9]*@?(amdocs)\\.com$", message="Email should be of the format 'Name.Surname@Amdocs.com'")])
+    address = StringField('Address',
+                          validators=[DataRequired()], default='A-24, Karan Gharonda, Sainikwadi, Vadgaonsheri, Pune 411014')
+    mobile_number = StringField('Mobile Number',
+                                 validators=[DataRequired(), Length(min=10, max=10), Regexp('\d{10}', message='Only 10 digit numbers')],
+                                 default='9595959700')
+    gift_image = FileField('Upload gift image', validators=[InputRequired(), DataRequired()])
     submit = SubmitField('Register')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email',validators=[InputRequired(), DataRequired(), Email()])
